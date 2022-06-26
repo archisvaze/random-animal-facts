2a3f7ec356ffda3dd5c1c3d150db89b0a1e7c771
archis <vazearchis@gmail.com>
2022-06-26 22:17:26 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+function fetchData() {
+    fetch("https://zoo-animal-api.herokuapp.com/animals/rand")
+        .then(response => response.json())
+        .then(data => {
+            let animal = data.name;
+            let scientificName = data.latin_name;
+            let habitat = data.habitat;
+            let lifeSpan = data.lifespan;
+            let animalType = data.animal_type;
+            let location = data.geo_range;
+            let image = data.image_link;
+
+
+            document.querySelector(".animal-name").innerHTML = animal;
+            document.querySelector(".animal-type").innerHTML = "Type: " + animalType;
+            document.querySelector(".lifespan").innerHTML = "Lifespan: " + lifeSpan + " years";
+            document.querySelector(".habitat").innerHTML = "Habitat: " + habitat;
+            document.querySelector(".geo_range").innerHTML = "Found in: " + location;
+            document.querySelector(".scientific-name").innerHTML = `"${scientificName}"`;
+
+            document.body.style.backgroundImage = `url("${image}")`
+        })
+
+        document.querySelector(".new-animal").classList.add("hide");
+        setTimeout(()=>{
+            document.querySelector(".new-animal").innerHTML = "differnt animal?";
+            document.querySelector(".new-animal").classList.remove("hide");
+        }, 4000)
+}
+
+// fetchData();
+
+
